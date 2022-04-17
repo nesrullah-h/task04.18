08f42ef67f895d0eb352a7adc889e3cb610e9499
Nesrullah Hacizade <nesrullah.hacizade@gmail.com>
2022-04-18 01:41:59 +0400

    created company

---
+++ Company/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Company.UTILS;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await GetAllEmployees(); 
+        }
+        public async static Task GetAllEmployees()
+        {
+            using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+            {
+                connection.Open();
+                string command = "SELECT * FROM Employee";
+                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                {
+                    SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Fullname: {reader["Fullname"]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Company/UTILS/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.UTILS
+{
+    internal class Constants
+    {
+        public static string connectionString = @"Data Source=WIN-7PA7VL1R1HI\SQLEXPRESS;Initial Catalog=Company;"
+              + "Integrated Security=true;";
+        
+    }
+}
